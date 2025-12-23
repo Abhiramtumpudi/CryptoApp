577918930d695d6c930b08886ed46114a2caddaf
Abhiram <abhiramt1988@gmail.com>
2025-12-23 20:10:57 +0530

    coin imageService

--- CryptoApp/Service/CoinImageService.swift
+++ CryptoApp/Service/CoinImageService.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import Combine
+import SwiftUI
+
+
+
+class CoinImageService  {
+    
+    @Published var image : UIImage? = nil
+    private var imageSubscription : AnyCancellable?
+    let coin : CoinModel
+    
+    init(coin : CoinModel) {
+        self.coin = coin
+        getImage()
+    }
+    
+    private func getImage() {
+        
+        guard let url  = URL(string: coin.image) else { return }
+        
+        imageSubscription =  NetworkManager.download(url: url)
+            .tryMap({ (data)-> UIImage? in
+                return UIImage(data: data)
+            })
+            .sink(receiveCompletion: NetworkManager.handleCompletion ,
+                receiveValue: ({ [weak self] (retunedImage) in
+                self?.image = retunedImage
+                self?.imageSubscription?.cancel()
+            })
+        
+    )}
+}
